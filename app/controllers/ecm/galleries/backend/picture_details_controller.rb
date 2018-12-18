bdb1b74236e4a828dc68486aa2c08fd8bb1069e6
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-12-18 15:21:21 +0100

    Tons of fixes and improvements.

--- app/controllers/ecm/galleries/backend/picture_details_controller.rb
+++ app/controllers/ecm/galleries/backend/picture_details_controller.rb
@@ -3,6 +3,9 @@ module Ecm
     module Backend
       class PictureDetailsController < Itsf::Backend::Resource::BaseController
         include ResourcesController::Sorting
+        include ResourcesController::BatchActionsConcern
+        include ResourcesController::ActsAsListConcern
+        include ResourcesController::ActsAsPublishedConcern
 
         helper Twitter::Bootstrap::Components::Rails::V3::ComponentsHelper
 
@@ -12,12 +15,16 @@ module Ecm
 
         private
 
+        def load_collection_scope
+          super.joins(:picture_gallery).order(picture_gallery_id: :asc, position: :asc)
+        end
+
         def after_destroy_location
           last_location
         end
 
         def permitted_params
-          params.require(:picture_detail).permit(:title, :description)
+          params.require(:picture_detail).permit(:title, :description, :picture_gallery_id, :published)
         end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Ecm::Galleries::Backend::Engine.routes.draw do
   resources :picture_galleries do
     post :toggle_published, on: :member
   end
-  resources :picture_details
+
+  resources :picture_details do
+    post :destroy_many, on: :collection
+    post :reposition, on: :member
+    post :toggle_published, on: :member
+  end
 
   root to: 'home#index'
 end
